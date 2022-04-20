44ad49aa52784dd79be05e031eced46a10251523
Hikari Ardhiansya <19102105@ittelkom-pwt.ac.id>
2022-04-20 16:52:58 +0700

    Tugas 5

---
+++ app/src/main/java/com/hikariardhiansya_19102105/practice5activity/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hikariardhiansya_19102105.practice5activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
